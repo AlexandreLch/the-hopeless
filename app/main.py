4d69d9eb8e15f6297842f7e8209a5db378969cdf
Sami Noirot <sami.noirot@gmail.com>
2021-04-20 23:47:44 +0200

    Add: screen creation + basic movement logic

--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
+import pygame
 from model.player import Player
 
-player = Player("Sami")
+pygame.init()
 
+size = width, height = 800, 600
+speed = 2
+black = 0, 0, 0
 
-print(player.user_name)
-print(player.health)
+screen = pygame.display.set_mode(size)
 
-player.move()
+bob = pygame.image.load("resources/bob.png")
+bob_rect = bob.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                bob_rect = bob_rect.move(10, 0)
+            if event.key == pygame.K_q:
+                bob_rect = bob_rect.move(-10, 0)
+            if event.key == pygame.K_z:
+                bob_rect = bob_rect.move(0, -10)
+            if event.key == pygame.K_s:
+                bob_rect = bob_rect.move(0, 10)
+
+    screen.fill(black)
+    screen.blit(bob, bob_rect)
+    pygame.display.flip()
--- app/model/player.py
+++ app/model/player.py
@@ -1,9 +1,13 @@
 class Player:
     health = 100
-    fighting = 0
-    mecanics = 0
-    cook = 0
-    navigation = 0
+    hunger = 100
+    damage = 10
+    speed = 2
+
+    technician_skill = 0
+    admin_skill = 0
+    ship_battle_skill = 0
+    navigator_skill = 0
 
     def __init__(self, user_name):
         self.user_name = user_name
